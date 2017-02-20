50991d2b502aa021ba61cb558cd189129f510d2b
dengwubogit <627498099@qq.com>
2017-02-20 13:37:22 +0800

    初次修改

--- ch_security/src/main/java/com/wisely/ChSecurityApplication.java
+++ ch_security/src/main/java/com/wisely/ChSecurityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChSecurityApplication {
-
+	// ASDASDASDASADRTE B
 	public static void main(String[] args) {
 		SpringApplication.run(ChSecurityApplication.class, args);
 	}
